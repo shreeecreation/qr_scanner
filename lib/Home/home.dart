51c6e409049e6ed01117605ff9ecc94cd4100da4
shreeecreation <shreeecreation720@gmail.com>
2022-07-27 22:15:44 +0545

    Managed the home page

--- lib/Home/home.dart
+++ lib/Home/home.dart
@@ -5,6 +5,38 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: _appBar(),
+      body: _bodyview(),
+    );
   }
+}
+
+AppBar _appBar() {
+  return AppBar(
+    title: const Text("Qr Code Usuage"),
+  );
+}
+
+Column _bodyview() {
+  return Column(
+    children: [
+      _bodyImage(),
+      _twobutton(
+          () => {
+                // ignore: avoid_print
+                print("jadu"),
+              },
+          "")
+    ],
+  );
+}
+
+Image _bodyImage() {
+  return Image.network(
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1200px-QR_code_for_mobile_English_Wikipedia.svg.png");
+}
+
+ElevatedButton _twobutton(final Function()? onpressed, final String text) {
+  return ElevatedButton(onPressed: onpressed, child: Text(text));
 }
